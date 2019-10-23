af8c3c495fe1785d3c23f7f8e39298224b7f260a
Kaung Myat Han <arkarhtethan@gmail.com>
2019-10-23 10:46:29 +0630

    Learning Dart Day 1

---
+++ day_1.dart
@@ -0,0 +1,143 @@
+void main() {
+  /*
+
+    this is another kind of comment
+
+  */
+
+  // this is my first line of code
+
+  // print("Hello from dart");
+
+  // print("This is my first application");
+
+  // print(30 / 5);
+
+  // print(true);
+
+  // learning data types
+
+  // Numbers : int
+
+  // int score = 23;
+
+  // var count = 23; // it is referred as integer automatically
+
+  // int hexValue = 0xEAEBAEE;
+
+  // // Numbers : double
+
+  // double percentage = 93.5;
+
+  // var percent = 94.3333;
+
+  // double exponents = 1.42e5;
+
+  // string
+
+  // String name = "Henry";
+  // var company = "Google";
+
+  // // Boolean
+
+  // bool isValid = true;
+  // var isAlive = false;
+
+  // var empty;
+
+  // print(empty); // all data type is object so null
+
+  // print(score);
+  // print(isValid);
+
+  // print(hexValue);
+  // print(exponents);
+
+  // String s1 = "This is some string";
+
+  // String s2 = 'This is some string';
+
+  // String s3 = "This is some long text"
+  //             "This is also another text";
+
+  // String name = "Kaung Myat Han";
+
+  // int age = 23;
+
+  // String message = "My name is $name and I am $age years old. The length of my name is ${name.length}";
+
+  // print(message);
+
+  // final cityName = "Mandalay";
+  // // final String cityName = "Mandalay";
+
+  // print(cityName);
+
+  // const PI = 3.14;
+
+  // print(PI);
+
+  // var salary = 15000;
+
+  // if (salary > 20000) {
+  //   print("Congratulation you got promotion");
+  // } else {
+  //   print("You need to work hard !");
+  // }
+
+  // var mark = 70;
+
+  // if (mark >= 90 && mark < 100) {
+  //   print("A+");
+  // } else if (mark >= 80 && mark < 90) {
+  //   print("A");
+  // } else if (mark >= 70 && mark < 80) {
+  //   print("B");
+  // } else if (mark >= 60 && mark < 70) {
+  //   print("C");
+  // } else if (mark > 30 && mark < 60) {
+  //   print("C");
+  // } else if (mark < 30) {
+  //   print("Fail");
+  // } else {
+  //   print("INvalid mark please try again");
+  // }
+
+  // int a = 2;
+  // int b = 3;
+
+  // if (a < b) {
+  //   print("$a is smaller");
+  // } else {
+  //   print("$b is smaller");
+  // }
+
+  // a < b ? print("$a is smaller") : print("$b is smaller");
+
+  // int smallerNumber = a < b ? a : b;
+
+  // print("Smaller Number is $smallerNumber");
+
+  // String name ;
+
+  // String nameToPrint = name ?? "Guest User";
+
+  // print(nameToPrint);
+
+  // String grade = 'A';
+
+  // switch (grade) {
+  //   case 'A':
+  //     print("Excellent grade of A");
+  //     break;
+  //   case 'B':
+  //     print("Very Good");
+  //     break;
+  //   case 'C':
+  //     print("Work hard");
+  //     break;
+  //   default:
+  //     print("Invalid grade");
+  //     break;
+  // }
+}
